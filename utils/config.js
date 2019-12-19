8456f137f930f1e74555702cdbde9d05445b8c47
admin <313500463@qq.com>
2019-12-19 10:19:42 +0800

    Initial Commit

---
+++ utils/config.js
@@ -0,0 +1,13 @@
+
+class Config{
+    constructor(){
+
+    }
+}
+
+// Config.restUrl = 'REST API 基地址';
+// Config.restUrl = 'http://z.cn/api/v1/';
+Config.restUrl = 'https://www.tpthink.cn/api/v1/';
+Config.onPay=true;  //是否启用支付
+
+export {Config};
